b88f216d33259debe3ad4a46cbb9db4ce32c7169
Aireos <alex.anderson@ucas-edu.net>
2025-01-22 12:45:04 -0700

    started coding store_tickets function
    by adding what remaining options they have to store and how it will run differently based on how many they have stored so far.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 #Nicholas, Aaron, Alex, and Yenesis Music Festival Project
-
-#This is Alex's place to code, right?
+def store_tickets():
+  remaining_options = ["seated,", "vip,", "grass"]
+  while True:
+    try: 
+      ticket_type = input("What ticket type do you want to store,", remaining_options[0], remaining_options[1], "or", remaining_options[2])
+    except:
+      try:
+        ticket_type = input("What ticket type do you want to store,", remaining_options[0], "or", remaining_options[1])
+      except:
+        ticket_type = input("You are now storing,", remaining_options[0], "tickets.")
+    
+        
+  
